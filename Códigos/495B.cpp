a5580ff719cf876ac4d48762f548c08187e4efc5
giovanysilva93 <69771585+giovanysilva93@users.noreply.github.com>
2021-02-14 11:30:47 -0300

    Update 495B.cpp

--- Códigos/495B.cpp
+++ Códigos/495B.cpp
@@ -13,21 +13,28 @@ int main(){
   sort(vet.begin(),vet.end());
   int dif = vet[n-1] - vet[0]; 
   cout<<dif<<" ";
-  int menor = 0,maior = 0;
+  unsigned long long int menor = 0,maior = 0;
   for(int i = 0;i < n;i++){
     if(vet[i] != vet[0]){
-      break;
+      break;                        
     }
     menor++;
   }
+
   for(int j = n-1;j >= 0;j--){
     if(vet[j] != vet[n-1]){
       break;
     }
     maior++;
   }
-  cout<<menor*maior<<endl;
+  
+  if(menor == n){
+    unsigned long long int resp = (maior*(maior-1))/2;
+    cout<<resp<<endl;
+  }else{
+    unsigned long long int resp = menor*maior;
+    cout<<resp<<endl;
+  }
 
 }
-
-               
+          
